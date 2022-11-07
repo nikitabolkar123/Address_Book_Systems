2022875df5c859228bb17436dcae08118860c557
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-06 23:54:09 -0800

    Added UC2

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -13,7 +13,8 @@ namespace AddressBook
             while (check)
             {
                 Console.WriteLine("Please select the Options :\n" +
-                    "1)Enter Details to add Contact in List\n");
+                    "1)Enter Details to add Contact in List\n" +
+                    "2)Add new Contact, Display and Stores contact in the list\n");
 
                 int option = Convert.ToInt32(Console.ReadLine());
 
@@ -22,8 +23,21 @@ namespace AddressBook
                     case 1:
 
                         AddressBook addobj = new AddressBook();
+                        Console.WriteLine("::::::::::::::::::::::::::");
                         addobj.createContact();
                         break;
+
+                    case 2:
+
+                        AddressBook addobj1 = new AddressBook();
+                        Console.WriteLine("::::::::::::::::::::::::::");
+                        addobj1.createContact();
+                        Console.WriteLine("::::::::::::::::::::::::::");
+                        Console.WriteLine("Entered Details of Person given as :");
+                        addobj1.Display();
+                        addobj1.addPerson();
+                        break;
+
 
                     default:
                         Console.WriteLine("Please select proper option");
