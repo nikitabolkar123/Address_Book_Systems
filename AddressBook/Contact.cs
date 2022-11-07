9c51e475c5832746602d1eaa893aefd699b4b631
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-06 23:47:26 -0800

    Added UC1

---
+++ AddressBook/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    public class Contact
+    {
+
+        //Using get and set method to access 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public long PhoneNo { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public string Email { get; set; }
+
+    }
+}
