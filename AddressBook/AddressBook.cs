6c6491a2d6c23b2ae20b62186b04dd1b76e3f25f
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-07 00:15:18 -0800

    Added UC4

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -39,6 +39,7 @@ namespace AddressBook
             Console.WriteLine("8)Enter Email-Id");
             contact.Email = Console.ReadLine();
 
+            //created addressBook list use to store 
             addressBook.Add(contact);
 
         }
@@ -52,7 +53,8 @@ namespace AddressBook
 
             foreach (Contact contact in addressBook)
             {
-                if (contact.FirstName == newcontact.FirstName)
+                //ToLower() added which convert any String into lower case
+                if (contact.FirstName.ToLower() == newcontact.FirstName.ToLower())
                 {
                     Console.WriteLine("Person with this Name Already Exists");
                     return;
@@ -82,6 +84,94 @@ namespace AddressBook
             addressBook.Add(newcontact);
         }
 
+        public void EditContact()
+        {
+            Console.WriteLine("Please Enter Name of Person to Edit");
+            string FirstName = Console.ReadLine();
+
+            foreach (Contact contact in addressBook)
+            {
+                if (contact.FirstName.ToLower() == FirstName.ToLower())
+                {
+                    Console.WriteLine("First Name Matches, please Enter Details to Edit ");
+                    Console.WriteLine("Select options to Edit Details :\n" +
+                        "1) Last Name\n" + "2) Address\n" + "3) City\n" +
+                        "4) State\n" + "5) Zip Code\n" + "6) Phone Number\n" + "7) E-mail\n");
+
+                    int option = Convert.ToInt32(Console.ReadLine());
+
+                    switch (option)
+                    {
+                        case 1:
+
+                            Console.WriteLine("Enter Last Name");
+                            contact.LastName = Console.ReadLine();
+                            break;
+
+                        case 2:
+
+                            Console.WriteLine("Enter Address");
+                            contact.Address = Console.ReadLine();
+                            break;
+
+                        case 3:
+
+                            Console.WriteLine("Enter City");
+                            contact.City = Console.ReadLine();
+                            break;
+
+                        case 4:
+
+                            Console.WriteLine("Enter State");
+                            contact.State = Console.ReadLine();
+                            break;
+
+                        case 5:
+
+                            Console.WriteLine("Enter Zip Code");
+                            contact.Zip = Convert.ToInt32(Console.ReadLine());
+                            break;
+
+                        case 6:
+
+                            Console.WriteLine("Enter Phone Number");
+                            contact.PhoneNo = Convert.ToInt64(Console.ReadLine());
+                            break;
+
+                        case 7:
+
+                            Console.WriteLine("Enter E-mail");
+                            contact.Email = Console.ReadLine();
+                            break;
+
+                        default:
+                            Console.WriteLine("Please Enter proper option");
+                            break;
+
+                    }
+
+                }
+                Console.WriteLine("No Contact Details found");
+            }
+        }
+
+        public void DeleteContact()
+        {
+            Console.WriteLine("Please Enter Name of Person to Edit");
+            string FirstName = Console.ReadLine();
+
+            foreach (Contact contact in addressBook)
+            {
+                if (contact.FirstName.ToLower() == FirstName.ToLower())
+                {
+                    addressBook.Remove(contact);
+                    Console.WriteLine("Entered First Name is Deleted from the List");
+                    return;
+                }
+            }
+            Console.WriteLine("Contact not Found ");
+        }
+
         //Display method 
         public void Display()
         {
@@ -99,3 +189,5 @@ namespace AddressBook
         }
     }
 }
+
+
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -14,7 +14,9 @@ namespace AddressBook
             {
                 Console.WriteLine("Please select the Options :\n" +
                     "1)Enter Details to add Contact in List\n" +
-                    "2)Add new Contact, Display and Stores contact in the list\n");
+                    "2)Add new Contact, Display and Stores contact in the list\n" +
+                    "3)Edit Details of Contact\n" +
+                    "4)Remove Contact Details from the List\n");
 
                 int option = Convert.ToInt32(Console.ReadLine());
 
@@ -38,15 +40,100 @@ namespace AddressBook
                         addobj1.addPerson();
                         break;
 
+                    case 3:
+
+                        AddressBook addobj2 = new AddressBook();
+                        Console.WriteLine("::::::::::::::::::::::::::");
+
+                        bool check1 = true;
+
+                        while (check1)
+                        {
+                            Console.WriteLine("Follow Steps to add details:\n" +
+                               "1) To addContact\n" + "2) To Edit Contact\n" +
+                               "3) To Display Contact\n");
+
+                            int select = Convert.ToInt32(Console.ReadLine());
+                            switch (select)
+                            {
+                                case 1:
+                                    addobj2.addPerson();
+                                    Console.WriteLine(":::::::::::::::::::::::::::;");
+                                    break;
+
+                                case 2:
+                                    addobj2.EditContact();
+                                    Console.WriteLine(":::::::::::::::::::::::::::;");
+                                    break;
+
+                                case 3:
+                                    addobj2.Display();
+                                    Console.WriteLine(":::::::::::::::::::::::::::;");
+                                    break;
+
+                                default:
+
+                                    check = !check;
+                                    break;
+                            }
+                        }
+                        break;
+
+                    case 4:
+
+                        AddressBook addobj3 = new AddressBook();
+                        Console.WriteLine("::::::::::::::::::::::::::");
+
+                        bool check2 = true;
+
+                        while (check2)
+                        {
+                            Console.WriteLine("Follow Steps to add details:\n" +
+                               "1) To addContact\n" + "2) To Edit Contact\n" +
+                               "3) To Remove Contact\n" + "4) To Display Contact\n");
+
+                            int select = Convert.ToInt32(Console.ReadLine());
+                            switch (select)
+                            {
+                                case 1:
+                                    addobj3.addPerson();
+                                    Console.WriteLine(":::::::::::::::::::::::::::;");
+                                    break;
+
+                                case 2:
+                                    addobj3.EditContact();
+                                    Console.WriteLine(":::::::::::::::::::::::::::;");
+                                    break;
+
+                                case 3:
+                                    addobj3.DeleteContact();
+                                    Console.WriteLine(":::::::::::::::::::::::::::;");
+                                    break;
+
+                                case 4:
+                                    addobj3.Display();
+                                    Console.WriteLine(":::::::::::::::::::::::::::;");
+                                    break;
+
+
+                                default:
+
+                                    check = !check;
+                                    break;
+                            }
+                        }
+                        break;
 
                     default:
                         Console.WriteLine("Please select proper option");
                         break;
                 }
                 check = false;
+                Console.WriteLine("Try Again ");
                 break;
             }
         }
     }
 
 }
+
