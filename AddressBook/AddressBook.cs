c4cd7c532447847d21f882481bc93a3c292d6aa4
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-07 00:03:45 -0800

    Added UC3

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -4,97 +4,171 @@ using System.Text;
 
 namespace AddressBook
 {
-    public class AddressBook
+    namespace AddressBookSystem
     {
-        //Ceating List to store contacts
-        public List<Contact> addressBook = new List<Contact>();
-
-        public void createContact()
+        public class AddressBook
         {
-            //instance creating of Contact class
+            //Ceating List to store contacts
+            public List<Contact> addressBook = new List<Contact>();
 
-            Contact contact = new Contact();
-
-            Console.WriteLine("1) Enter First Name :");
-            contact.FirstName = Console.ReadLine();
-
-            Console.WriteLine("2)Enter Last Name");
-            contact.LastName = Console.ReadLine();
-
-            Console.WriteLine("3)Enter Address");
-            contact.Address = Console.ReadLine();
-
-            Console.WriteLine("4)Enter City Name");
-            contact.City = Console.ReadLine();
-
-            Console.WriteLine("5)Enter State");
-            contact.State = Console.ReadLine();
-
-            Console.WriteLine("6)Enter Zip Code");
-            contact.Zip = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("7)Enter Phone Number");
-            contact.PhoneNo = Convert.ToInt64(Console.ReadLine());
-
-            Console.WriteLine("8)Enter Email-Id");
-            contact.Email = Console.ReadLine();
-
-            addressBook.Add(contact);
-
-        }
-
-        public void addPerson()
-        {
-            Contact newcontact = new Contact();
-
-            Console.WriteLine("Enter First Name of Person :");
-            newcontact.FirstName = Console.ReadLine();
-
-            foreach (Contact contact in addressBook)
+            public void createContact()
             {
-                if (contact.FirstName == newcontact.FirstName)
+                //instance creating of Contact class
+
+                Contact contact = new Contact();
+
+                Console.WriteLine("1) Enter First Name :");
+                contact.FirstName = Console.ReadLine();
+
+                Console.WriteLine("2)Enter Last Name");
+                contact.LastName = Console.ReadLine();
+
+                Console.WriteLine("3)Enter Address");
+                contact.Address = Console.ReadLine();
+
+                Console.WriteLine("4)Enter City Name");
+                contact.City = Console.ReadLine();
+
+                Console.WriteLine("5)Enter State");
+                contact.State = Console.ReadLine();
+
+                Console.WriteLine("6)Enter Zip Code");
+                contact.Zip = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("7)Enter Phone Number");
+                contact.PhoneNo = Convert.ToInt64(Console.ReadLine());
+
+                Console.WriteLine("8)Enter Email-Id");
+                contact.Email = Console.ReadLine();
+
+                addressBook.Add(contact);
+
+            }
+
+            public void addPerson()
+            {
+                Contact newcontact = new Contact();
+
+                Console.WriteLine("Enter First Name of Person :");
+                newcontact.FirstName = Console.ReadLine();
+
+                foreach (Contact contact in addressBook)
                 {
-                    Console.WriteLine("Person with this Name Already Exists");
-                    return;
+                    if (contact.FirstName == newcontact.FirstName)
+                    {
+                        Console.WriteLine("Person with this Name Already Exists");
+                        return;
+                    }
+                }
+                Console.WriteLine("2)Enter Last Name");
+                newcontact.LastName = Console.ReadLine();
+
+                Console.WriteLine("3)Enter Address");
+                newcontact.Address = Console.ReadLine();
+
+                Console.WriteLine("4)Enter City Name");
+                newcontact.City = Console.ReadLine();
+
+                Console.WriteLine("5)Enter State");
+                newcontact.State = Console.ReadLine();
+
+                Console.WriteLine("6)Enter Zip Code");
+                newcontact.Zip = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("7)Enter Phone Number");
+                newcontact.PhoneNo = Convert.ToInt64(Console.ReadLine());
+
+                Console.WriteLine("8)Enter Email-Id");
+                newcontact.Email = Console.ReadLine();
+
+                addressBook.Add(newcontact);
+            }
+
+            public void EditContact()
+            {
+                Console.WriteLine("Please Enter Name of Person to Edit");
+                string FirstName = Console.ReadLine();
+
+                foreach (Contact contact in addressBook)
+                {
+                    if (contact.FirstName == FirstName)
+                    {
+                        Console.WriteLine("First Name Matches, please Enter Details to Edit ");
+                        Console.WriteLine("Select options to Edit Details :\n" +
+                            "1) Last Name\n" + "2) Address\n" + "3) City\n" +
+                            "4) State\n" + "5) Zip Code\n" + "6) Phone Number\n" + "7) E-mail\n");
+
+                        int option = Convert.ToInt32(Console.ReadLine());
+
+                        switch (option)
+                        {
+                            case 1:
+
+                                Console.WriteLine("Enter Last Name");
+                                contact.LastName = Console.ReadLine();
+                                break;
+
+                            case 2:
+
+                                Console.WriteLine("Enter Address");
+                                contact.Address = Console.ReadLine();
+                                break;
+
+                            case 3:
+
+                                Console.WriteLine("Enter City");
+                                contact.City = Console.ReadLine();
+                                break;
+
+                            case 4:
+
+                                Console.WriteLine("Enter State");
+                                contact.State = Console.ReadLine();
+                                break;
+
+                            case 5:
+
+                                Console.WriteLine("Enter Zip Code");
+                                contact.Zip = Convert.ToInt32(Console.ReadLine());
+                                break;
+
+                            case 6:
+
+                                Console.WriteLine("Enter Phone Number");
+                                contact.PhoneNo = Convert.ToInt64(Console.ReadLine());
+                                break;
+
+                            case 7:
+
+                                Console.WriteLine("Enter E-mail");
+                                contact.Email = Console.ReadLine();
+                                break;
+
+                            default:
+                                Console.WriteLine("Please Enter proper option");
+                                break;
+
+                        }
+
+                    }
+                    Console.WriteLine("No Contact Details found");
                 }
             }
-            Console.WriteLine("2)Enter Last Name");
-            newcontact.LastName = Console.ReadLine();
 
-            Console.WriteLine("3)Enter Address");
-            newcontact.Address = Console.ReadLine();
-
-            Console.WriteLine("4)Enter City Name");
-            newcontact.City = Console.ReadLine();
-
-            Console.WriteLine("5)Enter State");
-            newcontact.State = Console.ReadLine();
-
-            Console.WriteLine("6)Enter Zip Code");
-            newcontact.Zip = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("7)Enter Phone Number");
-            newcontact.PhoneNo = Convert.ToInt64(Console.ReadLine());
-
-            Console.WriteLine("8)Enter Email-Id");
-            newcontact.Email = Console.ReadLine();
-
-            addressBook.Add(newcontact);
-        }
-
-        //Display method 
-        public void Display()
-        {
-            foreach (Contact contact in addressBook)
+            //Display method 
+            public void Display()
             {
-                Console.WriteLine(contact.FirstName);
-                Console.WriteLine(contact.LastName);
-                Console.WriteLine(contact.Address);
-                Console.WriteLine(contact.City);
-                Console.WriteLine(contact.State);
-                Console.WriteLine(contact.Zip);
-                Console.WriteLine(contact.PhoneNo);
-                Console.WriteLine(contact.Email);
+                foreach (Contact contact in addressBook)
+                {
+                    Console.WriteLine(contact.FirstName);
+                    Console.WriteLine(contact.LastName);
+                    Console.WriteLine(contact.Address);
+                    Console.WriteLine(contact.City);
+                    Console.WriteLine(contact.State);
+                    Console.WriteLine(contact.Zip);
+                    Console.WriteLine(contact.PhoneNo);
+                    Console.WriteLine(contact.Email);
+                }
             }
         }
     }
